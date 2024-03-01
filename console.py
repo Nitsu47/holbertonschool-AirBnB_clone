bb9e8e8429ba491b11960fa726002a558ab2b21b
Nitsu47 <agustraversa2004@gmail.com>
2024-03-01 20:27:01 +0000

    basic console with quit, EOF and help

---
+++ console.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Creates the command interpreter with basic cmd commands"""
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """defines all basic commands of the console"""
+
+    prompt = '(hbnb) '
+    h_list = ['Quit - command to exit the programm',
+              'EOF - command to exit the program']
+
+    def do_quit(self, *args):
+        """exits the console"""
+        return True
+
+    def do_EOF(self, *args):
+        """also exits the console"""
+        return True
+
+    def do_help(self, *args):
+        """shows availables help topics"""
+        print(self.h_list)
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
