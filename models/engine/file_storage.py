7940dc31e5f05c6c2e1d9bc54a85d5d018fba7e0
Bigguvix <nahufeola@hotmail.com>
2024-03-01 08:02:09 -0800

    added a class to serializes instances to a JSON file

---
+++ models/engine/file_storage.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""Creates a class that serializes 
+instances to a JSON file
+"""
+from models.base_model import BaseModel
+import json
+import os
+
+class FileStorage:
+    
+    __file_path = "file.json"
+    __objects = dict()
+    
+    def __init__(self):
+        pass
+
+    def all(self):
+        return self.__objects
+    
+    def new(self, obj):
+        key = f"{obj.__class__.__name__}.{obj.id}"
+        self.__objects[key] = obj
+
+    def save(self):
+        """serializes __objects to the JSON file"""
+        data = {}
+        for k, v in self.__objects.items():
+            data[k] = v.to_dict()
+        with open(self.__file_path, 'w') as f:
+            json.dump(data, f)
+    
+    def reload(self):
+        data_dict = {}
+        classes = {
+            "BaseModel": BaseModel
+        }
+        try:
+            with open(self.__file_path, 'r') as f:
+                data_dict = json.load(f)
+                for k, v in data_dict.items():
+                    self.__objects[k] = classes[v["__class__"]](**v)
+        except FileNotFoundError:
+            pass
